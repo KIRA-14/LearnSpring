819cc94f789527adc73244c5ed296eafd8488731
Ravikiran <Ravikiran@192.168.2.13>
2019-07-20 14:06:38 +0530

    updated

--- src/main/java/qa/kira/dev/controllercontroller/topic/TopicDataserviuce.java
+++ src/main/java/qa/kira/dev/controllercontroller/topic/TopicDataserviuce.java
@@ -1,10 +1,8 @@
 package qa.kira.dev.controllercontroller.topic;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.repository.CrudRepository;
 
 
-@Configuration
 public interface TopicDataserviuce extends CrudRepository<Topic,String>{
 
 }
